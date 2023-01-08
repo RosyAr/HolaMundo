9dcd7bba0abc96048b875f9202ef397943d55685
RosyAr <119703765+RosyAr@users.noreply.github.com>
2023-01-08 16:33:29 -0600

    se genero el primer commit del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
+import { BodyComponent } from './components/body/body.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
